3b704b2700e83d9439ad4420d2467638e6942ec4
feyruz-loopcamp <feyruz@loopcamp.io>
2023-05-21 11:25:19 -0400

    latest

---
+++ src/test/java/com/loop/test/day5_testNG_dropdown/T000_testNG_with_selenium.java
@@ -0,0 +1,4 @@
+package com.loop.test.day5_testNG_dropdown;
+
+public class T000_testNG_with_selenium {
+}
---
+++ src/test/java/com/loop/test/day5_testNG_dropdown/T0_staleElementException.java
@@ -0,0 +1,54 @@
+package com.loop.test.day5_testNG_dropdown;
+
+import com.loop.test.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+public class T0_staleElementException {
+    public static void main(String[] args) {
+
+
+
+//        3. Click to “Add Element” button
+//        4. Verify “Delete” button is displayed after clicking.
+//        5. Click to “Delete” button.
+//        6. Verify “Delete” button is NOT displayed after clicking.
+//        USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
+
+
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //2. Go to http://the-internet.herokuapp.com/add_remove_elements/
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+
+        //3. Click to “Add Element” button
+        WebElement addButton = driver.findElement(By.xpath("//button[contains(text(),'Add Element')]"));
+        addButton.click();
+
+        WebElement deleteButton = driver.findElement(By.xpath("//button[contains(text(),'Delete')]"));
+        if (deleteButton.isDisplayed()){
+            System.out.println("Delete button is displayed ---> TEST PASS");
+        } else {
+            System.out.println("Delete button is NOT displayed ---> TEST FAIL");
+        }
+
+        deleteButton.click();
+
+        try {
+            deleteButton.isDisplayed();
+        } catch(StaleElementReferenceException e){
+            System.out.println("Exception handled");
+            System.out.println("Element is not displayed anymore");
+            System.out.println("Delete button is NOT displayed ---> TEST PASS");
+        }
+        driver.quit();
+    }
+}
